beadc40529bf2b7b945de8b5a7ec31c9b938dc25
dorgold46 <dgoldenberg@lightricks.com>
2023-10-03 13:28:03 +0300

    fix parsing

--- src/parsing.py
+++ src/parsing.py
@@ -1,10 +1,22 @@
 import csv
 import re
 
-
 def parse_file(file):
     # Split the chat history into lines
     lines = file.split("\n")
+
+    # Determine the date format by checking the first non-empty line
+    date_format = None
+    for line in lines:
+        if line:
+            if re.match(r"\d+\.\d+\.\d+", line):
+                date_format = "d.m.y"
+            elif re.match(r"\d+/\d+/\d+", line):
+                date_format = "m/d/y"
+            break
+
+    if not date_format:
+        raise ValueError("Unable to determine date format")
 
     # Prepare the CSV file
     with open("parsed_chat.csv", "w", encoding="utf-8", newline='') as csvfile:
@@ -13,8 +25,12 @@ def parse_file(file):
         csvwriter.writerow(["date", "time", "user", "text"])
 
         for line in lines:
-            # Using regular expressions to match the date, time, user and text
-            match = re.search(r"(\d+\.\d+\.\d+), (\d+:\d+) - (.*?): (.*)", line)
+            if date_format == "d.m.y":
+                pattern = re.compile(r"(\d+\.\d+\.\d+), (\d+:\d+) - (.*?): (.*)")
+            else:
+                pattern = re.compile(r"(\d+/\d+/\d+), (\d+:\d+) - (.*?): (.*)")
+
+            match = pattern.search(line)
 
             if match:
                 date, time, user, text = match.groups()
